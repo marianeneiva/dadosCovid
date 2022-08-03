e7bbb0ff3324803d0fbf2c6321fd7cb94c05dfbe
Mariane Neiva <47704014+marianeneiva@users.noreply.github.com>
2022-08-03 20:50:09 -0300

    Add files via upload

--- codigobase.py
+++ codigobase.py
@@ -9,7 +9,9 @@ Original file is located at
 
 import pandas as pd
 import plotly.express as px
+import streamlit as st 
 
+#streamlit run codigoBase.py
 
 #LENDO O DATASET
 df = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv')
@@ -18,12 +20,13 @@ df = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases
 df = df.rename(columns={'newDeaths': 'Novos óbitos','newCases': 'Novos casos','deaths_per_100k_inhabitants': 'Óbitos por 100 mil habitantes','totalCases_per_100k_inhabitants':'Casos por 100 mil habitantes'})
 
 #SELECÃO DO ESTADO
-state  = 'MS'
 estados = list(df['state'].unique())
+state = st.sidebar.selectbox('Qual estado?', estados)
 
 #SELEÇÃO DA COLUNA
-column ='Casos por 100 mil habitantes'
+#column ='Casos por 100 mil habitantes'
 colunas = ['Novos óbitos','Novos casos','Óbitos por 100 mil habitantes','Casos por 100 mil habitantes']
+column = st.sidebar.selectbox('Qual tipo de informação?', colunas)
 
 #SELEÇÃO DAS LINHAS QUE PERTECEM AO ESTADO 
 df = df[df['state'] == state]
@@ -31,4 +34,9 @@ df = df[df['state'] == state]
 fig = px.line(df, x="date", y=column, title=column + ' - ' + state)
 fig.update_layout( xaxis_title='Data', yaxis_title=column.upper(), title = {'x':0.5})
 
-fig.show()
+st.title('DADOS COVID - BRASIL')
+st.write('Nessa aplicação, o usuário tem a opção de escolher o estado e o tipo de informação para mostrar o gráfico. Utilize o menu lateral para alterar a mostragem.')
+
+st.plotly_chart(fig, use_container_width=True)
+
+st.caption('Os dados foram obtidos a partir do site: https://github.com/wcota/covid19br')
