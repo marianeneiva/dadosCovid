51bb7890c49120de06cf3c4fa573c77fdcc334f5
Mariane Neiva <47704014+marianeneiva@users.noreply.github.com>
2022-08-03 21:00:50 -0300

    Update codigobase.py

--- codigobase.py
+++ codigobase.py
@@ -21,12 +21,12 @@ df = df.rename(columns={'newDeaths': 'Novos óbitos','newCases': 'Novos casos','
 
 #SELECÃO DO ESTADO
 estados = list(df['state'].unique())
-state = st.sidebar.selectbox('Qual estado?', estados)
+state = st.selectbox('Qual estado?', estados)
 
 #SELEÇÃO DA COLUNA
 #column ='Casos por 100 mil habitantes'
 colunas = ['Novos óbitos','Novos casos','Óbitos por 100 mil habitantes','Casos por 100 mil habitantes']
-column = st.sidebar.selectbox('Qual tipo de informação?', colunas)
+column = st.selectbox('Qual tipo de informação?', colunas)
 
 #SELEÇÃO DAS LINHAS QUE PERTECEM AO ESTADO 
 df = df[df['state'] == state]
